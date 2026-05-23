16345dab03b590f64b4afde9bd22ee2f28722877
Owen Nich 11 <nicholas.sentosa@binus.ac.id>
2026-05-23 12:40:27 +0700

    fix: disable static caching for public pages

--- src/app/(public)/disaster-info/page.tsx
+++ src/app/(public)/disaster-info/page.tsx
@@ -1,6 +1,8 @@
 import DisasterInfoPage from "./components/DisasterInfoPage"
 import { getDisasters } from "@/services/disasterServices"
 
+export const dynamic = "force-dynamic"
+
 export default async function Page() {
 
   const disasters = await getDisasters()
--- src/app/(public)/mitigation-guide/page.tsx
+++ src/app/(public)/mitigation-guide/page.tsx
@@ -2,6 +2,8 @@ import MitigationGuidePage from "./components/MitigationGuidePage"
 import { getDisasters } from "@/services/disasterServices"
 import { getMitigationGuides } from "@/services/mitigationServices"
 
+export const dynamic = "force-dynamic"
+
 export default async function Page() {
 
   const disasters = await getDisasters()
